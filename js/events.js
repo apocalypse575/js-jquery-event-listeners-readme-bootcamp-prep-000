25a56a5eb9431e668e177e1913d2bff9db7dddb6
apocalypse575 <apocalypse575@gmail.com>
2017-11-11 13:10:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,32 @@
 //define functions here
 
+function getIt() {
+  $("p").click(function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $(document).on('load', function(){
+    $('#img').append('#tasty')
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('g was pressed!')
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
